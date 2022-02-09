26ee11366a682a9ac43bddc5c3629fac559b7f68
Patrick <patrick@workos.com>
2022-02-09 09:00:48 -0600

    Add Webhook test endpoint to net directory sync app (#2)
    * Comment out HTTP Redirects
    
    * Comment out HTTP Redirects
    
    * Add Webhook endpoint
    
    * Newline added
    
    * Newline added

--- dotnet-directory-sync-example/Startup.cs
+++ dotnet-directory-sync-example/Startup.cs
@@ -40,7 +40,7 @@ namespace WorkOS.DSyncExampleApp
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-            app.UseHttpsRedirection();
+           // app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
